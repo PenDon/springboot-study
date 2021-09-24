1c44843343e05f3eddc67df8cd90e92f40facb91
dongpenh <13122995191@163.com>
2021-09-24 16:53:08 +0800

    ...

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,15 +1,12 @@
 package com.example.demo.controller;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    @ResponseBody
+    @GetMapping("/api/hello")
     public String home() {
         return "Hello World!";
     }
